6b6909138018aa7f35e852a46ac01a026433e139
xujintao <2199272070@qq.com>
2024-12-10 21:06:01 +0800

    [1]：编译出能返回指定数值的程序

--- src/main.c
+++ src/main.c
@@ -5,12 +5,17 @@ int main(int Argc, char **Argv)
 {
     // 判断传入程序的参数是否为2个，Argv[0]为程序名称，Argv[1]为传入的第一个参数
     if(Argc != 2) {
-        // Argv[0]是程序名
+        // 异常处理，提示参数数量不对。
+        // fprintf，格式化文件输出，往文件内写入字符串
+        // stderr，异常文件（Linux一切皆文件），用于往屏幕显示异常信息
+        // %s，字符串
         fprintf(stderr, "%s: invalid number of arguments\n", Argv[0]);
+        // 程序返回值不为0时，表示存在错误
         return 1;
     }
-
+    // 声明一个全局main段，同时也是程序入口段
     printf(".global main\n");
+    // main段标签
     printf("main:\n");
     // li为addi别名指令，加载一个立即数到寄存器中
     // 传入程序的参数为str类型，因为需要转换为需要int类型，
